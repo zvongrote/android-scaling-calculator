1504b40d12513f020fc4c6c385f99a97eb5719cb
Zach von Grote <zvongrote@gmail.com>
2021-04-16 00:22:59 -0700

    Added logic for scaling to a different density.
    Created a wrapper class for a dimension to hold width and height that can be scaled by some ratio. Needs testing still.

--- scaling_calculator.py
+++ scaling_calculator.py
@@ -1,4 +1,30 @@
+from __future__ import annotations
 import argparse
+from typing import Callable
+
+
+class Dimension:
+    def __init__(self, width: float, height: float) -> None:
+        self.width = width
+        self.height = height
+
+    def scale(self, ratio: float) -> Dimension:
+        scaled_width = self.width * ratio
+        scaled_height = self.height * ratio
+
+        return Dimension(scaled_width, scaled_height)
+
+
+def scale_dimension(dimension: Dimension, base_density: str, target_density: str, densities: dict[str, int], round: Callable[[float], float]) -> Dimension:
+    scaling_ratio = densities[target_density] / densities[base_density]
+
+    scaled_dimension = dimension.scale(scaling_ratio)
+
+    scaled_dimension.width = round(scaled_dimension.width)
+    scaled_dimension.height = round(scaled_dimension.height)
+
+    return scaled_dimension
+
 
 # Parse input
 parser = argparse.ArgumentParser(
@@ -20,3 +46,9 @@ parser.add_argument("-r", "--rounding-mode",
                     help="How to round the results")
 
 args = parser.parse_args()
+
+# Create the base dimension
+base_dimensions = Dimension(int(args.width), int(args.height))
+
+# Map containing the name and scaling ratio for each density
+densities = {"l": 3, "m": 4, "h": 6, "xh": 8, "xxh": 12, "xxxh": 16}
